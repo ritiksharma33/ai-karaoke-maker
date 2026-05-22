874b912c70c1b13ebd24f2e0aa26592f2d11019d
ritiksharma33 <ritikeducationalid@gmail.com>
2026-05-22 09:50:05 +0530

    feat: add audio processing services for separation and conversion

---
+++ backend/app/services/demucs_service.py
@@ -0,0 +1,19 @@
+import subprocess
+import os
+
+OUTPUT_DIR = 'app/outputs'
+
+def seperate_audio(file_path):
+    command =['demucs','--two-stems=vocals',file_path] 
+    subprocess.run(command,check=True)
+    filename=os.path.splitext(os.path.basename(file_path))[0]
+    instrumental_path = (
+        f"separated/htdemucs/{filename}/no_vocals.wav"
+    )
+    vocals_path = (
+        f"separated/htdemucs/{filename}/vocals.wav"
+    )
+    return {
+        "instrumental":instrumental_path,
+        "vocals":vocals_path
+    }
